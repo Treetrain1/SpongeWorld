0197198ecd3199fa127bdd2ca6b72de240e866cc
Treetrain1 <treetrain1@gmail.com>
2022-08-07 18:45:20 -0500

    more invasive

--- src/main/java/net/frozenblock/spongeworld/mixin/VanillaSurfaceRulesMixin.java
+++ src/main/java/net/frozenblock/spongeworld/mixin/VanillaSurfaceRulesMixin.java
@@ -8,7 +8,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(VanillaSurfaceRules.class)
@@ -21,15 +20,14 @@ public abstract class VanillaSurfaceRulesMixin {
 
 	private static final MaterialRules.MaterialRule SPONGE = block(Blocks.SPONGE);
 
-	@ModifyVariable(method = "createDefaultRule", at = @At("STORE"), ordinal = 8)
-	private static MaterialRules.MaterialRule overworld(MaterialRules.MaterialRule materialRule) {
-
-		return SPONGE;
+	@Inject(method = "createDefaultRule", at = @At("HEAD"), cancellable = true)
+	private static void overworld(boolean surface, boolean bedrockRoof, boolean bedrockFloor, CallbackInfoReturnable<MaterialRules.MaterialRule> cir) {
+		cir.setReturnValue(SPONGE);
+		cir.cancel();
 	}
 
 	@Inject(method = "createNetherSurfaceRule", at  = @At("HEAD"), cancellable = true)
 	private static void nether(CallbackInfoReturnable<MaterialRules.MaterialRule> cir) {
-
 		cir.setReturnValue(SPONGE);
 		cir.cancel();
 	}
